654cbfcfd99eba8d1b0ab7d6210c2e8d649bb35f
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-24 20:18:04 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+// task 6: write add,sub,multiply,divide operations,each results in eight
+function numberEight()
+{
+    console.log(4+4);
+    console.log(16-8);
+    console.log(4*2);
+    console.log(16/2);
+}
+numberEight()
